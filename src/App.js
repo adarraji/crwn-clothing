3b3a69211bdcdf461d646a132bb9b183b327bad4
adarraji <a.darraji@outlook.com>
2023-11-15 21:21:26 -0500

    using array and map for categories

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React from 'react'
 
+const categories = [
+  {
+    id: 1,
+    title: "Hats",
+  },
+  {
+    id: 2,
+    title: "Jackets",
+  },
+  {
+    id: 3,
+    title: "Sneakers",
+  },
+  {
+    id: 4,
+    title: "Womens",
+  },
+  {
+    id: 5,
+    title: "Mens",
+  },
+]
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className="categories-container">
+      {categories.map(category => (
+        <div key={category.id} className="category-container">
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2>{category.title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
 
