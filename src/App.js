3227282f405203e19d83341054b59836c8a1d73a
adarraji <a.darraji@outlook.com>
2023-11-16 13:49:00 -0500

    added nav and outlet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./app.scss"
 import Home from "./routes/home/Home"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Outlet } from "react-router-dom"
 
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navaifation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />} >
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   )
 }
