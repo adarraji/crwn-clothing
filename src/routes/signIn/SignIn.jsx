e679da7a1ad2e35167cedcacc392b9559e9d7c43
adarraji <a.darraji@outlook.com>
2023-11-19 18:34:24 -0500

    updated gitignore

--- src/routes/signIn/SignIn.jsx
+++ src/routes/signIn/SignIn.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { signInWithGooglePopup } from "../../utils/firebase/firebase"
 
 const SignIn = () => {
+    const logGoogleUser = async () => {
+        const response = await signInWithGooglePopup();
+        console.log(response)
+    }
     return (
-        <div><h1>SignIn</h1></div>
+        <div>
+            <h1>SignIn</h1>
+            <button onClick={logGoogleUser}>
+                Sign In With Google Popup
+            </button>
+        </div>
     )
 }
 
