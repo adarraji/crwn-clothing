bb6e4cd260f35930b4f4724d96ce018d02e2fd05
adarraji <a.darraji@outlook.com>
2023-11-24 13:21:17 -0500

    fix

--- src/routes/signIn/SignIn.jsx
+++ src/routes/signIn/SignIn.jsx
@@ -4,15 +4,14 @@ import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/f
 const SignIn = () => {
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user)
-        // console.log(userDocRef)
+        await createUserDocumentFromAuth(user)
     }
 
     return (
         <div>
             <h1>SignIn</h1>
             <button onClick={logGoogleUser}>Sign In With Google Popup</button>
-            <Signup/>
+            <Signup />
         </div>
     )
 }
