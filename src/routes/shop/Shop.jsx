fec19c627af5f24f4b6261bf07149274aa9af331
adarraji <a.darraji@outlook.com>
2023-12-11 15:01:43 -0500

    added styls

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,3 +1,4 @@
+import "./shop.scss"
 import { useContext } from "react"
 import SHOP_DATA from "../../shop-data.json"
 import { ProductsContext } from "../../context/productsContext"
@@ -6,7 +7,7 @@ import ProductCard from "../../components/productCard/ProductCard"
 const Shop = () => {
     const { products } = useContext(ProductsContext)
     return (
-        <div>
+        <div className="products-container">
             {SHOP_DATA.map(product => (
                 <ProductCard key={product.id} product={product} />
             ))}
