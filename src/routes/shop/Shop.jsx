1bbf82e7e2d8361be1f6759bb7029841cad7d223
adarraji <a.darraji@outlook.com>
2023-12-12 18:52:10 -0500

    fix

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,11 +1,11 @@
 import "./shop.scss"
-import { useContext } from "react"
+// import { useContext } from "react"
 import SHOP_DATA from "../../shop-data.json"
-import { ProductsContext } from "../../context/productsContext"
+// import { ProductsContext } from "../../context/productsContext"
 import ProductCard from "../../components/productCard/ProductCard"
 
 const Shop = () => {
-    const { products } = useContext(ProductsContext)
+    // const { products } = useContext(ProductsContext)
     return (
         <div className="products-container">
             {SHOP_DATA.map(product => (
