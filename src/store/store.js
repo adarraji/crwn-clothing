0c631e73bdd2385ec5423df35fe47cdeda0c5a1b
adarraji <a.darraji@outlook.com>
2024-02-04 16:31:31 -0500

    usinf redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,10 @@
 import { compose, legacy_createStore, applyMiddleware } from "redux"
 // import logger from "redux-logger"
+import { persistStore, persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
+
 import { rootReducer } from "./root-reducer"
+
 
 const loggerMiddleware = (store) => (next) => (action) => {
     if (!action.type) {
@@ -15,11 +19,22 @@ const loggerMiddleware = (store) => (next) => (action) => {
     console.log("next State: ", store.getState())
 }
 
+const persistConfig = {
+    key: "root",
+    storage,
+    blacklist: ["user"]
+}
+
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const middlewares = [loggerMiddleware]
 
 const compsedEnhancers = compose(applyMiddleware(...middlewares))
 
-export const store = legacy_createStore(rootReducer, undefined, compsedEnhancers)
+export const store = legacy_createStore(persistedReducer, undefined, compsedEnhancers)
+
+export const persistor = persistStore(store)
 
 
 
