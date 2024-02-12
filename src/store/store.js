b4b8ba93ee9a6cc04814f18b29ab99fd674626a0
adarraji <a.darraji@outlook.com>
2024-02-11 19:57:28 -0500

    using redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,37 +1,31 @@
-// import { compose, legacy_createStore, applyMiddleware } from "redux"
 import { configureStore } from "@reduxjs/toolkit"
 import logger from "redux-logger"
-// import { persistStore, persistReducer } from "redux-persist"
-// import storage from "redux-persist/lib/storage"
+import { persistStore, persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
 
 import { rootReducer } from "./root-reducer"
 
+// Setting redux-persist
+const persistConfig = {
+    key: "root",
+    storage,
+    blacklist: ["user"]
+}
 
-
-// const persistConfig = {
-//     key: "root",
-//     storage,
-//     blacklist: ["user"]
-// }
-
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer)
+// setting persisted reducer 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(Boolean)
 
-// const composeEnhancer = (process.env.NODE_ENV !== "production" && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
-
-// const compsedEnhancers = composeEnhancer(applyMiddleware(...middleWares))
-
 export const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false,
     }).concat(middleWares)
 })
 
 
-// export const persistor = persistStore(store)
+export const persistor = persistStore(store)
 
 
 
