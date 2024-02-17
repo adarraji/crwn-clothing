0caf39339ce5f9bba1fa41fc5ed150a42f1455d0
adarraji <a.darraji@outlook.com>
2024-02-17 15:16:54 -0500

    changed from getCategoriesAndDocuments() to getCategoriesAndDocuments("categories")

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -12,7 +12,7 @@ const Shop = () => {
 
     useEffect(() => {
         const getCategoriesMap = async () => {
-            const categoriesArray = await getCategoriesAndDocuments()
+            const categoriesArray = await getCategoriesAndDocuments("categories")
             dispatch(setCategories(categoriesArray))
         }
 
