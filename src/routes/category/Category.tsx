dd742ea6369d97f479679a9be543dacbd59fd76b
adarraji <a.darraji@outlook.com>
2024-03-06 20:04:55 -0500

    typing category

--- src/routes/category/Category.tsx
+++ src/routes/category/Category.tsx
@@ -1,18 +1,22 @@
 import { useParams } from "react-router-dom"
 import { useState, useEffect, Fragment } from "react"
-import { CategoryContainer, Title } from "./Category.styles.jsx"
-import ProductCard from "../../components/productCard/ProductCard"
+import { CategoryContainer, Title } from "./Category.styles"
+import ProductCard from "../../components/productCard/ProductCard.jsx"
 import Spinner from "../../components/spinner/Spinner.jsx"
-
 import { selectCategoriesMap, selectCategoriesIsLoading } from "../../store/categories/category.selector"
 import { useSelector } from "react-redux"
+import { CategoryItem } from "../../store/categories/category.types"
+
+type CategoryRouteParams = {
+  category: string
+}
 
 
 const Category = () => {
-  const { category } = useParams()
-  const [products, setProducts] = useState([])
+  const { category } = useParams<keyof CategoryRouteParams>() as CategoryRouteParams
+  const [products, setProducts] = useState<CategoryItem[]>([])
   const categoriesMap = useSelector(selectCategoriesMap)
-  const isLoading = useSelector(selectCategoriesIsLoading)  
+  const isLoading = useSelector(selectCategoriesIsLoading)
 
   useEffect(() => {
     setProducts(categoriesMap[category])
